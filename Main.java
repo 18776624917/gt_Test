957cd361a5686860128a4c3b9d549720d05b94ec
18776624917 <1902684864@qq.com>
2020-12-19 21:07:16 +0800

    lqq

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double math=64,english=76.5,chinese=66;
+        ImportantUniversity univer=new ImportantUniversity();
+        univer.enterRule(math,english,chinese);
+    }
+}
